1d4b50b6e561950b8c6fe263b104dd11c8414757
ThomasSnowden37 <thomassnowden37@gmail.com>
2025-03-31 19:17:53 -0400

    tset

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
