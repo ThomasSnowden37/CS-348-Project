094e54ccdb6b360a2805888574df511ab26dcbfb
ThomasSnowden37 <thomassnowden37@gmail.com>
2025-03-31 20:55:16 -0400

    database

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from database import Tool, db, Location
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 
@@ -13,8 +14,10 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
@@ -22,22 +25,21 @@ if __name__ == '__main__':
     app.run()
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    name = StringField('name')
+    type = StringField('type')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
+        new_tool = Tool(name = form.name.data, type = form.type.data)
+        try:
+            db.session.add(new_tool)
+            db.session.commit()
+        except:
+            return jsonify({'status': 0, 'message': 'Tool could not be added to database.'}), 500
+        return jsonify({'status': 1, 'message': f'Tool {form.name.data} creation successful!'}), 200
+    return render_template('register.html', form = form)
 
 @app.route("/")
 def my_form():
@@ -63,3 +65,7 @@ def handle_add_tool():
                     "id": new_tool.id,
                     "message": f"Item {new_tool.name} added successfully."
                     }), 200
+
+@app.route("/tool", methods=['GET'])
+def handle_tool():
+    return render_template('my-form.html')
