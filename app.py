33d76c6f64239d1d6d6d16aef47af72bc22d0d06
ThomasSnowden37 <thomassnowden37@gmail.com>
2025-03-31 20:12:04 -0400

    form tests

--- app.py
+++ app.py
@@ -21,6 +21,24 @@ with app.app_context():
 if __name__ == '__main__':
     app.run()
 
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
 @app.route("/")
 def my_form():
     return render_template('my-form.html')
